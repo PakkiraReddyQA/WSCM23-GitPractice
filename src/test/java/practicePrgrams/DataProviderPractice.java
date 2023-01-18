5926f20bc0b781edc30e19ca67eeb3e221bf13f4
pakki <pakki@YatamPakkiraReddy>
2023-01-18 11:04:53 +0530

    commited Automation Lead

---
+++ src/test/java/practicePrgrams/DataProviderPractice.java
@@ -0,0 +1,46 @@
+package practicePrgrams;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataProviderPractice {
+	@Test(dataProvider = "phone")
+	public void dataproviderTest(String phone,int price,String model)
+	{
+		System.out.println(phone+" "+price+" "+model);
+		
+	}
+	@DataProvider(name="phone")
+	public Object[][] getData()
+	{
+		Object[][] data=new Object[2][3];
+		
+		data[0][0]="samsung";
+		data[0][1]=15000;
+		data[0][2]="su234";
+		
+		data[1][0]="i phone";
+		data[1][1]=25000;
+		data[1][2]="i45";
+		
+		return data;
+	
+	}
+	@DataProvider(name="accesiors")
+	public Object[][] getData1()
+	{
+		Object[][] data=new Object[2][3];
+		
+		data[0][0]="samsung";
+		data[0][1]=15000;
+		data[0][2]="su234";
+		
+		data[1][0]="i phone";
+		data[1][1]=25000;
+		data[1][2]="i45";
+		
+		return data;
+	
+	}
+
+}
