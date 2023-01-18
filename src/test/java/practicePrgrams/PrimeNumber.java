43d253a1b8c705afb8d796f29a816a6610a84895
pakki <pakki@YatamPakkiraReddy>
2023-01-18 13:07:16 +0530

    commit and push PrimeNumber

---
+++ src/test/java/practicePrgrams/PrimeNumber.java
@@ -0,0 +1,21 @@
+package practicePrgrams;
+
+import org.testng.annotations.Test;
+
+public class PrimeNumber {
+	@Test
+	public void prime()
+	{
+		int num=7,count=0;
+		for(int i=1;i<=num;i++)
+		{
+			if(num%i==0)
+				count++;
+		}
+		if(count==2)
+			System.out.println(num+" is a prime number");
+		else
+			System.out.println(num+" is not prime number");
+	}
+
+}
