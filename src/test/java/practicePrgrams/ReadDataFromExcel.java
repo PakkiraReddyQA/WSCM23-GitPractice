57310017d9188a21837a5f2f206110c0c64d8596
pakki <pakki@YatamPakkiraReddy>
2023-01-18 12:11:33 +0530

    commit and push By Lead

---
+++ src/test/java/practicePrgrams/ReadDataFromExcel.java
@@ -0,0 +1,24 @@
+package practicePrgrams;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.testng.annotations.Test;
+
+public class ReadDataFromExcel {
+	@Test
+	public void readDataFromExcel() throws EncryptedDocumentException, IOException
+	{
+		FileInputStream fis=new FileInputStream("src\\test\\resources\\TestData.xlsx");
+		Workbook book = WorkbookFactory.create(fis);
+		Sheet sh = book.getSheet("Organization");
+		String value = sh.getRow(4).getCell(2).getStringCellValue();
+		System.out.println(value);
+	}
+	
+
+}
