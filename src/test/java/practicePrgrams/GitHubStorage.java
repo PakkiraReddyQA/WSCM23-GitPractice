474518c7ecbc960a00c1e2dccbe9775298de2895
pakki <pakki@YatamPakkiraReddy>
2023-01-19 14:34:02 +0530

    commit and push by pakkira TE

--- src/test/java/practicePrgrams/GitHubStorage.java
+++ src/test/java/practicePrgrams/GitHubStorage.java
@@ -1,10 +1,17 @@
 package practicePrgrams;
 
+import org.testng.annotations.Test;
+
 public class GitHubStorage {
 	@Test
 	public void git()
 	{
 		System.out.println("gitHub");
+	}
+	@Test
+	public void ram()
+	{
+		System.out.println("jayaram");
 	}
 
 }
