f747231de9ea1319583163788aa5ad942061d48b
pakki <pakki@YatamPakkiraReddy>
2023-01-19 14:50:58 +0530

    commit and testsgit1,git2

--- src/test/java/practicePrgrams/GitHubStorage.java
+++ src/test/java/practicePrgrams/GitHubStorage.java
@@ -8,5 +8,14 @@ public class GitHubStorage {
 	{
 		System.out.println("gitHub");
 	}
-
+	@Test
+	public void git1()
+	{
+		System.out.println("gitHub1");
+	}
+	@Test
+	public void git2()
+	{
+		System.out.println("gitHub2");
+	}
 }
